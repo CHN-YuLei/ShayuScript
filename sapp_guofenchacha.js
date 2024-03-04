297c6b0c5748306584922aec578cf521f8376d66
YvLei <newyulei@163.com>
2024-03-04 18:35:32 +0800

    Add files via upload

---
+++ sapp_guofenchacha.js
@@ -0,0 +1,13 @@
+/*******************************
+[rewrite_local]
+^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/Ceshi.js
+[mitm] 
+hostname = *.tuanyougou.*
+*******************************/
+
+console.log($response.body);
+var obj = JSON.parse($response.body);
+obj.data.showAd = "0";
+obj.data.hasAdPrize = true;
+obj.data.hasAdPrize = 100;
+$done({body: JSON.stringify(obj)});
