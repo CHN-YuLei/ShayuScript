84e7391b0b34f9b868228b3063befef95f2ffab0
YvLei <newyulei@163.com>
2024-03-04 19:43:30 +0800

    Update sapp_guofenchacha.js

--- sapp_guofenchacha.js
+++ sapp_guofenchacha.js
@@ -8,6 +8,4 @@ hostname = *.tuanyougou.*
 console.log($response.body);
 var obj = JSON.parse($response.body);
 obj.data.showAd = "0";
-obj.data.hasAdPrize = true;
-obj.data.adPrizeAmount = 100;
 $done({body: JSON.stringify(obj)});
